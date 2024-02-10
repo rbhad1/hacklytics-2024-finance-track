48453717bea420c1d7ee7ebf3b28884e5b4c61e5
bpragya <bpragya@outlook.com>
2024-02-10 18:10:45 -0500

    css implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template_string
 import yfinance as yf
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,14 +10,52 @@ HTML_FORM = '''
 <head>
     <title>Portfolio Data</title>
 </head>
+
+
+<head>
+    <title>Banner Example</title>
+    <style>
+        .banner {
+        background-color: #4CAF50; /* Green background */
+        color: white; /* White text */
+        text-align: center; /* Centered text */
+        padding: 20px; /* Some padding */
+        font-size: 24px; /* Large text */
+    }
+    </style>
+</head>
+<body>  
+
+<div class="banner">
+    PANDA
+</div>
+</body>  
+
+<head>
+<style>
+    body {background-color: #add8e6;}
+</style>
+</head>
+
 <body>
-    <h2>Enter Ticker Symbols</h2>
+    <h2 style="color:blue;">Enter Ticker Symbols</h2>
+    
     <form method="POST">
-        <label for="tickers">Tickers (comma-separated, e.g., AAPL,MSFT,GOOGL):</label><br>
+        <label for="tickers">Tickers (comma-separated, e.g., AAPL, MSFT, GOOGL):</label><br>
         <input type="text" id="tickers" name="tickers"><br>
         <input type="submit" value="Submit">
     </form>
 </body>
+
+<head>
+    <style>
+        h2 {
+            font-size: 20px;
+        }
+    </style>
+</head>
+
+
 </html>
 '''
 
@@ -32,7 +71,16 @@ def home():
                 net_income_2022 = float(table[table.columns.values[1]].iloc[table.index == "Net Income"].to_string(index=False).strip())
                 net_income_2023 = float(table[table.columns.values[0]].iloc[table.index == "Net Income"].to_string(index=False).strip())
                 percent_change_net_income = (((net_income_2023 - net_income_2022) / net_income_2022) * 100)
-                data[ticker] = percent_change_net_income
+
+                gross_profit_2022 = float(
+                    table[table.columns.values[1]].iloc[table.index == "Gross Profit"].to_string(index=False).strip())
+                gross_profit_2023 = float(
+                    table[table.columns.values[0]].iloc[table.index == "Gross Profit"].to_string(index=False).strip())
+                percent_change_gross_profit = (((gross_profit_2023 - gross_profit_2022) / gross_profit_2022) * 100)
+                data[ticker] = {"net income": percent_change_net_income,
+                                "gross profit": percent_change_gross_profit}
+
+
             except Exception as e:
                 data[ticker] = f"Error retrieving data: {e}"
         return f'<pre>{data}</pre>'
